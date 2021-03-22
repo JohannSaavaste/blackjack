3eeb28afae96633087dbd50f98f457107d8d4989
RookieSoldat <62138072+RookieSoldat@users.noreply.github.com>
2021-03-22 19:07:54 +0200

    Update Mängija.java
    Lisasin juurde ässa liitmise

--- Mängija.java
+++ Mängija.java
@@ -13,7 +13,10 @@ public class Mängija{
     public int kaartideSumma() {
         int summa = 0;
         for (Kaart k: kaardid) {
-            summa += k.getVäärtus();
+            if(summa<=10 && ässasid==0&& k.getKirjeldus().equals("A")){
+                summa+=11;
+                this.ässasid++;
+            } else summa += k.getVäärtus();
         }
         return summa;
     }
@@ -22,12 +25,24 @@ public class Mängija{
         return kaartideSumma() > 21;
     }
 
+    public boolean kas21() {return  kaartideSumma()==21;}
+
+    public List<Kaart> getKaardid() {
+        return kaardid;
+    }
+
     public void lisaKaart(Kaart kaart) {
         kaardid.add(kaart);
-        if (kaart.getKirjeldus().split(" ")[1].equals("äss")) this.ässasid++;
+        if (kaart.getKirjeldus().equals("A")) this.ässasid++;
+        /*
         if (this.kasÜle()){
             //siin tuleks ässa väärtus teha 11 asemel üheks? vb
             //sp loemegi ässade arvu pakis et nende väärtust saaks vajadusel muuta
         }
+
+         */
     }
+
+
 }
+
