8591a9c99fb1177614e40fbe928f7914e49ae858
RookieSoldat <62138072+RookieSoldat@users.noreply.github.com>
2021-03-22 19:13:34 +0200

    Update Blackjack.java
    Uuendasin peaklassi ja lisasin testimiseks esimese kahe kaardi jagamise, kuid millegipärast viskab erindi kaardipakist kaardi eemaldamisel.

--- Blackjack.java
+++ Blackjack.java
@@ -3,12 +3,26 @@ import java.util.*;
 public class Blackjack {
 
     public static void main(String[] args) {
-        String[] mastid = {"ruutu", "risti", "ärtu", "poti"};
-        String[] väärtused = {"kaks", "kolm", "neli", "viis", "kuus", "seitse",
-                "kaheksa", "üheksa", "kümme", "poiss", "emand", "kuningas", "äss"};
+        String[] mastid = {"♦", "♣", "♥", "♠"};
+        String[] väärtused = {"2", "3", "4", "5", "6", "7",
+                "8", "9", "10", "J", "Q", "K", "A"};
 
         Kaart[] algnePakk = new Kaart[52];
         int lugeja = 0;
+        for (String mast: mastid){
+            for (int i = 0; i < väärtused.length; i++) {
+                String kirjeldus = väärtused[i];
+                if (i < 9)
+                    algnePakk[lugeja] = new Kaart(i+2, kirjeldus, mast);
+                else if (i == väärtused.length-1){
+                    algnePakk[lugeja] = new Kaart(1, kirjeldus, mast);
+                } else {
+                    algnePakk[lugeja] = new Kaart(10, kirjeldus,mast);
+                }
+                lugeja++;
+            }
+        }
+        /*
         for (int i = 0; i < mastid.length; i++) {
             for (int j = 0; j < väärtused.length; j++) {
                 String kirjeldus = mastid[i] + " " + väärtused[j];
@@ -22,22 +36,55 @@ public class Blackjack {
                 lugeja++;
             }
         }
+
+         */
+
         //kontrollisin sellega kas teeb õiged kaardid:
         //Arrays.stream(algnePakk).iterator().forEachRemaining(System.out::println);
 
-        List<Kaart> pakk = segaKaardid(algnePakk);
+        //for (Kaart k: pakk) System.out.println(k);
 
+        Mängija mängija = new Mängija();
+        Diiler diiler = new Diiler();
+        /*
         Scanner input = new Scanner(System.in);
         System.out.print("Teretulemast mängu Blackjack! Alustamiseks sisesta panus: ");
         int panus = input.nextInt();
         System.out.println("Sinu panus on " + panus);
 
+         */
+        String jätkamine = "jah";
+        while (jätkamine.equals("jah")){
+            System.out.println("Alustame uue mänguga");
+            List<Kaart> pakk = segaKaardid(algnePakk);
+            System.out.println(pakk);
+            for (int i = 0; i < 2; i++) {
+                Kaart mängija_kaart=pakk.get((int) (Math.random() * pakk.size()));
+                mängija.lisaKaart(mängija_kaart);
+                System.out.println(mängija_kaart);
+                pakk.remove(mängija_kaart);                            //siin viskab erindi
+
+                Kaart diileri_kaart=pakk.get((int) (Math.random() * pakk.size()));
+                diiler.lisaKaart(pakk.get((int) (Math.random() * pakk.size())));
+                pakk.remove(diileri_kaart);
+            }
+            System.out.println("Sinu kaardid: "+ mängija.getKaardid());
+            System.out.println("Diileri kaardid: "+ diiler.getKaardid());
+            System.out.println(pakk);
+        }
+
+
+
+
+
     }
+
 
     public static List<Kaart> segaKaardid(Kaart[] algnePakk) {
         List<Kaart> pakkListina = Arrays.asList(algnePakk);
         Collections.shuffle(pakkListina);
         return pakkListina;
     }
+
 
 }
